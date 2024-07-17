abc6b4fcdcfc837c68865348b781109452318b7d
username <useremail>
2024-07-16 22:55:31 -0700

    work

--- CIS261 Course Project/CIS261_Course_Project.py
+++ CIS261 Course Project/CIS261_Course_Project.py
@@ -1,1 +1,35 @@
+# Name
+# CIS___
+# Course Project Phase 1
 
+
+def employeeName():
+    name = input("Employee Name: ")
+    return name
+
+
+def totalHours():
+    t_hours = input("Total Hours: ")
+    return t_hours
+
+
+def hourlyRate():
+    hRate = input("Hourly Rate: ")
+    return hRate
+
+
+def incomeTaxRate():
+    it_rate = input("Income Tax Rate: ")
+    return it_rate
+
+
+
+def grossPay_incomeTax_netPay(totalHrs, hrlyRate, txRate):
+    grossPay = totalHrs * hrlyRate
+    incomeTax = grossPay * txRate
+    netPay = grossPay - incomeTax
+    return grossPay, incomeTax, netPay
+
+
+def display_info():
+    print()

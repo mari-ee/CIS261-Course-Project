8786d0f909785ef28e7bd7067d88cc7730325c2d
mari-ee <133037927+mari-ee@users.noreply.github.com>
2024-07-17 11:16:00 -0500

    Update CIS261_Course_Project.py
    Changes

--- CIS261 Course Project/CIS261_Course_Project.py
+++ CIS261 Course Project/CIS261_Course_Project.py
@@ -40,7 +40,7 @@ def grossPay_incomeTax_netPay(totalHrs, hrlyRate, txRate):
 
 
 def display_info(empName, hr_work, hr_rate, it_rate, grpay, inctax, netpay):
-    print("Employee Name: " + empName)
+    print("\nEmployee Name: " + empName)
     print("Hours Worked: " + f"{hr_work: ,.2f}")
     print("Hourly Rate: $" + f"{hr_rate: ,.2f}")
     print("Income Tax Rate: $" + f"{it_rate: ,.2f}")
@@ -50,7 +50,7 @@ def display_info(empName, hr_work, hr_rate, it_rate, grpay, inctax, netpay):
 
 
 def info_summary(t_emp, t_hours, t_gpay, t_tax, t_netPay):
-    print("Total Number of Employees: " + t_emp)
+    print("\nTotal Number of Employees: " + str(t_emp))
     print("Total Hours: " + f"{t_hours: ,.2f}")
     print("Total Gross Pay: $" + f"{t_gpay: ,.2f}")
     print("Total Tax: $" + f"{t_tax: ,.2f}")
@@ -59,7 +59,7 @@ def info_summary(t_emp, t_hours, t_gpay, t_tax, t_netPay):
 
 if __name__ == "__main__":
     e_name = employeeName()
-    if e_name.lower() != "end":
+    while e_name.lower() != "end":
         hours_worked = hoursWorked()
         hourly_rate = hourlyRate()
         income_taxrate = incomeTaxRate()
